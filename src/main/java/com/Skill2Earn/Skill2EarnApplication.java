fdc9809d85560ca1fdd9506c81e315e4998852d1
SulthanPatan07 <sulthanpatan786@gmail.com>
2025-07-31 14:38:48 +0530

    Sulthan

---
+++ src/main/java/com/Skill2Earn/Skill2EarnApplication.java
@@ -0,0 +1,13 @@
+package com.Skill2Earn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill2EarnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skill2EarnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Skill2Earn/Skill2EarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Skill2Earn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skill2EarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
